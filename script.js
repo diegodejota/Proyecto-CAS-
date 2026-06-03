55afcac71b46a5344387f4436c6a305fcfc0014a
Diego <flgodiegobustamante@gmail.com>
2026-06-03 15:36:10 -0400

    se arregla menu hamburguesa. Se ajusta hero section.

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ const hamburguesa = document.querySelector(".hamburguesa");
 
 const menu = document.querySelector(".menu");
 
+const cerrar = document.querySelector(".cerrar");
+
 hamburguesa.addEventListener("click", function(){
 
     menu.classList.toggle("activo");
 
 });
 
+cerrar.addEventListener("click", function(){
+
+    menu.classList.remove("activo");
+
+});
