45875e4d357489e712acf633235bdb363611dcb3
Diego <flgodiegobustamante@gmail.com>
2026-06-03 14:12:00 -0400

    Se agrega menu hamburguesa para celulares

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Javascript conectado correctamente");
+
+const hamburguesa = document.querySelector(".hamburguesa");
+
+const menu = document.querySelector(".menu");
+
+hamburguesa.addEventListener("click", function(){
+
+    menu.classList.toggle("activo");
+
+});
+
