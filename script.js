41948db94835dbe89655fe83591785a8043f953d
Diego <flgodiegobustamante@gmail.com>
2026-06-04 12:16:01 -0400

    botones de la navbar dirigen a las secciones. se agrega seccion quienes somos. se añade al menu hamburgesa fondo oscuro. ahora el menu hamburguesa se cierra cuando le hacen click a los enlaces

--- script.js
+++ script.js
@@ -6,14 +6,32 @@ const menu = document.querySelector(".menu");
 
 const cerrar = document.querySelector(".cerrar");
 
+const enlaces = document.querySelectorAll(".menu a");
+
+const overlay = document.querySelector(".overlay");
+
 hamburguesa.addEventListener("click", function(){
 
     menu.classList.toggle("activo");
+    overlay.classList.toggle("activo");
 
 });
 
 cerrar.addEventListener("click", function(){
 
     menu.classList.remove("activo");
+    overlay.classList.remove("activo");
 
 });
+
+enlaces.forEach(function(enlace){
+
+    enlace.addEventListener("click", function(){
+
+        menu.classList.remove("activo");
+        overlay.classList.remove("activo");
+
+    });
+
+});
+
