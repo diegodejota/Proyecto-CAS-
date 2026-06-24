f099e6ae497d155e699e09e24cac9b686ba4a9cf
Diego <flgodiegobustamante@gmail.com>
2026-06-24 13:34:37 -0400

    Se actualizan fotos a reales para la pagina. Se arreglan funciones al hero como mostrador de noticias

--- script.js
+++ script.js
@@ -14,6 +14,56 @@ const slides = document.querySelectorAll(".slide");
 
 let indice = 0;
 
+const heroImagen = document.querySelector(".slide");
+
+const heroTitulo = document.getElementById("hero-titulo");
+
+const heroTexto = document.getElementById("hero-texto");
+
+const heroBoton = document.getElementById("hero-boton");
+
+const noticias = [
+
+{
+    imagen: "Imagenes/hero.jpeg",
+    titulo: "CAS Autismo La Pintana",
+    texto: "Cada paso es un logro, cada proceso tiene su ritmo.",
+    boton: "Contáctanos",
+    link: "https://wa.me/56927653017"
+},
+
+{
+    imagen: "Imagenes/apadrinamiento.jpeg",
+    titulo: "Apadrina una terapia",
+    texto: "Con tu aporte ayudas a que niños y niñas accedan a terapias especializadas y continúen su proceso de desarrollo.",
+    boton: "Quiero ayudar",
+    link: "#"
+},
+
+{
+    imagen: "Imagenes/habilidades-sociales.jpeg",
+    titulo: "Taller de habilidades sociales",
+    texto: "Espacios grupales diseñados para fortalecer la comunicación, interacción y autonomía de nuestros usuarios.",
+    boton: "Conocer más",
+    link: "#actividades"
+},
+{
+    imagen: "Imagenes/Huerto.png",
+    titulo: "Taller de Huerto",
+    texto: "Actividad dirigidas a padres con el objetivo de embellecer nuestro centro y generar un espacio de encuentro y aprendizaje.",
+    boton: "Conocer más",
+    link: "#terapias"
+},
+{
+    imagen: "Imagenes/arreglos.png",
+    titulo: "Seguimos mejorando nuestro centro",
+    texto: "Se han realizado multiples arreglos en nuestro centro, con el objetivo de brindar un mejor servicio a nuestros usuarios y sus familias.",
+    boton: "Conocer más",
+    link: "#terapias"
+}
+
+];
+
 hamburguesa.addEventListener("click", function(){
 
     menu.classList.toggle("activo");
@@ -40,12 +90,33 @@ enlaces.forEach(function(enlace){
 });
 
 function cambiarSlide(){
-    slides[indice].classList.remove("activo");
-    indice++;
-    if(indice >= slides.length){
-        indice = 0;
-    }
-    slides[indice].classList.add("activo");
+
+    console.log("cambiando slide");
+
+    heroImagen.style.opacity = "0";
+
+    setTimeout(function(){
+
+        indice++;
+
+        if(indice >= noticias.length){
+            indice = 0;
+        }
+
+        heroImagen.src = noticias[indice].imagen;
+
+        heroTitulo.textContent = noticias[indice].titulo;
+
+        heroTexto.textContent = noticias[indice].texto;
+
+        heroBoton.textContent = noticias[indice].boton;
+
+        heroBoton.href = noticias[indice].link;
+
+        heroImagen.style.opacity = "1";
+
+    },500);
+
 }
 
-setInterval(cambiarSlide, 5000);
+setInterval(cambiarSlide, 8000);
